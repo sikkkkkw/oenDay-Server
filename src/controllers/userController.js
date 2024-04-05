55f02fbe81898c9e29e1cdc2616fc4238c23f0fd
sikkkkkw <gksmsla92@naver.com>
2024-04-05 15:50:24 +0900

    djdjdjdjdj

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -66,8 +66,8 @@ export const loginSuccess = async (req, res) => {
   try {
     const user= await User.findById(req.query.token)
     console.log(user);
-    if (req.query.token) {
-      res.send({ result: true, user: req.query.token, isLogin: true });
+    if (user) {
+      res.send({ result: true, user:user ,isLogin: true });
     } else {
       res.send({ result: true, isLogin: false });
     }
